229b153dfd576233b8c83a29b4ba0168322f4074
monika <aryamisti@gmail.com>
2025-03-27 17:30:35 +0530

    project pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import authService from './appwrite/auth'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import {login, logout} from './store/authSlice'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+const [loading, SetLoading] = useState(true)
+const dispatch = useDispatch()
 
-  return (
-    <>
-     <h1>appwrite app project</h1>
-    </>
-  )
+useEffect(() =>{
+   authService.getCurrentUser()
+   .then((userData) =>{
+    if(userData){
+      dispatch(login(userData))
+    }else {
+      dispatch(logout())
+    }
+   })
+   .finally(() => SetLoading(false))
+}, [])
+  return  !loading ? (
+    <div  className='min-h-screen flex flex-wrap content-between items-center bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
