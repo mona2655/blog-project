73ba79e9f2150a4da53cbcf7a99a632cae26ac25
monika <aryamisti@gmail.com>
2025-03-30 23:34:06 +0530

    postform optimization

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -8,6 +8,35 @@ export default function RTE({name, control,
   <div className='w-full'>
     {label && <label className='inline-block mb-1 pl-1'>
     {label} </label>}
+
+    <Controller
+    name={name || 'content'}
+    control={control}
+    render={({field: {onChange}}) => (
+      <Editor
+    initialValue='default value'
+    init={
+        {branding: false,
+            height: 500,
+            menubar: true,
+            plugins:[
+                'adlist autoLink lists link image charmap print preview',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table pase code help wordcount'
+            ],
+            toolbar:
+            'undo redo | formatselect | bold italic backcolor\
+            alignleft aligncenter alignright alignjustify|\
+            bullist numlist outdent indent | removeformat| help',
+            content_style:'body {font-family:Helvetica Arial,sans-serif; font-size:14px'
+
+        }
+    }
+
+    onEditorChange={onChange}
+    />
+    )}
+    />
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 
 export{
     Header,
     Footer,
     Container,
     LogoutBtn,
+    RTE,
 }
